8c55fc3dba4dae5422b1d3799947816025a23988
Brisset <corcinobrisset@gmail.com>
2021-11-23 22:02:30 -0500

    clase 1

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,15 @@
 
+console.log("hola mund")
+
+let number1 = prompt(parseInt("ingresa 1er número"))
+let number2 = prompt(parseInt("ingresa 2do número"))
+
+if (number1>number2 ) {
+    
+} if (number1<number2 ) {
+    
+} if (condition) {
+    
+} else {
+    
+}
