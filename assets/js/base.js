687558b686eead4b59234e18bd602bff9cb61cf1
Brisset <corcinobrisset@gmail.com>
2021-12-09 16:07:44 -0500

    setter y getter

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,39 +1,82 @@
 
-//////////////////clase-8 02-12-21/////////
-// ['hOla','munDo']
-// -> ['Hola','Mundo']
+let koder = {
+    name:"jorge",
+    lastName:"Camarillo",
+    average: [10, 10, 10, 8, 9],
+    age: 30,
 
-const capitalizeWithMap = (arr) => {
-	let arrMap =  []
-	arrMap= arr.map( (element) => {	
-		let string = `${element.slice(0,1).toUpperCase()}${element.slice(1).toLowerCase()}`
-		return string
-    })
-	return arrMap
+    getAge: function () {
+        return this.age
+    },
+
+    get ageKoder() {
+        return this.age
+    },
+
+    set ageKoder(age) {
+        this.age = age
+    },
+
+    get fullName() {
+        return `${ this.name } ${ this.lastName }`
+    },
+
+    set fullName(objFullName) {
+        this.name = objFullName[0]
+        this.lastName = objFullName[1]
+    },
+
+
 }
 
-let arr = ['hOlA', 'mUNdo','cOMo','eStaS']
-let arrCapitalizado = capitalizeWithMap(arr )
-//console.log(arrCapitalizado)
+// Metodos
+console.log( koder.getAge() )
 
-////////filter
-let arrayFilter =[1,2,3,4,5,6,7]
+// getters
+console.log('Edad inicial: ', koder.ageKoder)
+console.log('Nombre completo: ', koder.fullName)
 
-let arrForEach = arrayFilter.filter(element => {
-	return element %2 ===0
-});
-//console.log(arrForEach)
+// setters
+koder.ageKoder = 47
+koder.fullName = ['jorge luis', 'camarillo']
 
+// getter y setter de 
 /**
  * 
- * -> ['hola','mundo', 0, 5]
- * -> ['hola','mundo']
- * 1. funcion
- * 2. filtrar solo los que son de typo string
+ * 1. getter average
+ * 2. getter, setter IMC 
+ * 3. setter Score
+ * 4. setter newProperty
+ * 
+ * 
  */
-let arrayFil = ['hola','mundo', 0, 5,'como']
 
-let onlyString = arrayFil.filter(element => typeof element ==='string')
-console.log(onlyString)
- 
-/////otro 
+
+/**
+ * Estudiar DOM 
+ * CSS y HTML
+ */
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Agregar un metodo para calcular el IMC 
+koder.setGetIMC = function () { 
+    this.peso = 57 
+    this.altura = 1.50 
+    this.IMC = this.peso/this.altura**2 
+} 
+koder.setGetIMC() 
+
+// Agregar un metodo para Agregar un nuevo promedio 
+koder.setNewAve = function () { 
+    this.average.push(10) 
+} 
+koder.setNewAve() 
+// Agregar un metodo para agregar una nueva propiedad 
+koder.addNewProperty = function (key,value) { 
+    this[key] = value 
+} 
+koder.addNewProperty('nationality','Mexican') 
+
+
+console.log(koder) 
--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,176 +1,94 @@
-
 /**
- * Escribir un arrow function que compruebe
- * si una oración es un palindromo
- * hint: https://ricardometring.com/javascript-replace-special-characters
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
- */
-// large
-const isPalindrome = (sentence) => {
-    let sentenceNormalized = sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-    let sentenceWithoutSpaces = sentenceNormalized.replace(/\s/g,'')
-    let sentenceReversed = sentenceWithoutSpaces.split('').reverse().join('')
-    if(sentenceWithoutSpaces === sentenceReversed) {
-        return true
-    } else {
-        return false
-    }
-}
-// short
-// const isPalindrome = sentence => {
-//     let sentenceNormalized = sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/\s/g,'')
-//     let sentenceReversed = sentenceNormalized.split('').reverse().join('')
-//     return sentenceNormalized === sentenceReversed ? true : false
-// }
-
-/**
- * Escribir un arrow function que tome como parametro 2 arrays, 
- * y devuelva un numero con la suma total de esos 2 arrays.
- * arrayReduce( [1,2,3,4], [1,2]) 
- * -> 13
+ * Ejercicio 1.
+ * Dado un objeto inicial, hacer los siguientes puntos 
  * 
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
- * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+ * 1. Agregar el lenguaje 'Go' a la lista de lenguajes
+ * 2. Cambiar el nivel a 4
+ * 3. Eliminar la propiedad avatar
+ * 4. Agregar una nueva propiedad de edad y poner el valor de 30
+ * 5. Imprimir en consola todos los lenguajes dominados
+ * 6. Clonar el objeto en uno nuevo
+ * 7. Imprimir en consola el nuevo objeto
  * 
-*/
-const arrayReduce = (arrOne, arrTwo) => {
-    let newArr = arrOne.concat(arrTwo)
-    let arrAdd = 0
-    
-    // funcion tradicional
-    // newArr.forEach( function (item) {
-    //     arrAdd += item
-    // })
-
-    // arrow function
-    newArr.forEach( value => {
-        arrAdd = arrAdd + value
-    })
-
-    // newArr.forEach( value => arrAdd += value )
-    return arrAdd
-}
-
-
-let resultado = arrayReduce( [1,2], [3] )
-//console.log(resultado)
-// middle
-// const arrayReduce = (arrOne, arrTwo) => {
-//     let arrAdd =  0
-//     arrOne.concat(arrTwo).forEach( item => arrAdd += item )
-//     return arrAdd
-// }
-
-// short
-// const arrayReduce = (arrOne, arrTwo) => {
-//     let arrAdd =  0
-//     return arrOne.concat(arrTwo).forEach( item => arrAdd += item )
-// }
-
-// one line solution
-// const arrayReduce = (arrOne, arrTwo) => arrOne.concat(arrTwo).reduce( (acc, cv) => acc += cv )
-
-/**
- * Escribir una función que tome como parametro 2 arrays, y devuelva un array 
- * con los numeros repetidos en los 2 arrays
- * arrayReduce([1,2,3,4],[1,2,5]). 
- * -> [1,2]
-*/
-
-// large
-// const getRepeatItems =  (arrOne, arrTwo) => {
-//     let concatArr = arrOne.concat(arrTwo)
-//     let arrRepeated = []
-//     concatArr.forEach((element, index) =>  {
-//         // console.log( concatArr.indexOf(element), index)
-//         if( concatArr.indexOf(element) !== index ) {
-//             arrRepeated.push(element)
-//         }
-//     })
-//     return arrRepeated
-// }
-
-// middle
-// const getRepeatItems =  (arrOne, arrTwo) => {
-//     let arrRepeated = []
-//     arrOne.concat(arrTwo).forEach((element, index) =>  {
-//         concatArr.indexOf(element) !== index ? arrRepeated.push(element) : ''
-//     })
-//     return arrRepeated
-// }
-
-
-// Large filter
-// const getRepeatItems =  (arrOne, arrTwo) => {
-//     let arrRepeated = []
-//     arrRepeated = arrOne.concat(arrTwo).filter((element, index, arr) =>  {
-//         if( arr.indexOf(element) !== index ) {
-//             return element
-//         }
-//     })
-
-//     return arrRepeated
-// }
-
-// short filter
-// const getRepeatItems = (arrOne, arrTwo) => {
-//     return arrOne.concat(arrTwo).filter((element, index,arr) =>  {
-//         return arr.indexOf(element) !== index ? element : ''
-//     })
-// }
-
-// one line -- not recomended
-//const getRepeatItems = (arrOne, arrTwo) => arrOne.concat(arrTwo).filter((item, index,arr) => arr.indexOf(item) !== index ? item : '')
-//////////////////////////////////////////////////////Clase 01-12-21/////////////////////////////////////////
-
-// 1. Generar una function
-// que reciba un array como parametro
-// y devuelva solo los items que son pares
-// p.ej. onlyEvenValues( [1, 2, 3, 4] )
-// -> [2, 4]
-// Aplicar el metodo .forEach()
-// hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
-
-const onlyEvenValues = arrNumbers => {
-    // codigo aquí
-}
-
-
-
-// 2. function que reciba como parametro una array de strings
-// y devuelva la primer y ultima letra del string
-// de cada uno de los items del array 
-// -> firstAndLast (['hola', 'mundo'] )
-// -> ['ha', 'mo']
-// .forEach() o .map()
-
-const firstAndLast = arrStrings => {
-    // codigo aquí
-
-}
-
-/**
- * 3. Funcion que reciba un array de numeros
- * y devuelva el promedio
- * -> getAverage( [10, 8, 9, 7] )
- * -> 8.5
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
  */
 
-const getAverage = (arr) => {
+ let koder = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isMentor: true,
+    level: 3,
+    avatar: "https://picsum.photos/200/300"
+}
+
+let newOBj =  { ...koder }
+newOBj.age = 30
+console.log(newOBj)
+
+
+
+/**
+ * Ejercicio 2:
+ * Escribir una funcion que reciba un parametro
+ * Verificar si el parametro es un objeto
+ * o si es un array
+ * p.ej.  
+ * ·> isAnObjectOrArray( [1,2,3] )
+ * -> 'Es un array'
+ * ·> isAnObjectOrArray( {key:'value'} )
+ * -> 'Es un Objeto'
+ * ·> isAnObjectOrArray( 'Hola mundo' )
+ * -> 'No es un array ni un objeto'
+ * 
+ * 
+ * @params {Object}, [Array] ,'string' - Estructura que se quiere validar
+ * @return 'String' - mensaje de respuesta
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+ * 
+ */
+
+const isAnObjectOrArray = (element) => {
 
 }
 
+/**
+ * Ejercicio 3.
+ * Realizar una funcion que tome como parametro un objeto
+ * y devuelva un array de arrays con la siguiente estructura
+ * [ [key, value], [key, value] ]
+ * Resultado esperado: makePairs({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+ * 
+ * @params {Object} object - El objeto que queremos transformar
+ * @return [Array] - El array que se espera retornar
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ * 
+ * Se tiene que realizar 2 soluciones, 1 con Object.entries() y otra con .map()
+ */
+ const makePairs = (object) => Object.entries(object);
 
-//  4. funcion
-// dado un array de años, filtre los elementos
-// y devuelva un array con los años que son bisiesto
-// convertLeapYear( [1990, 2000, 2001, 2020] )
-// -> [2000,2020]
-// .forEach()
-// hint: https://docs.microsoft.com/es-es/office/troubleshoot/excel/determine-a-leap-year
+const makePairs2 = (object) => Object.keys(object).map((el) => [el, object[el] ]); 
 
-const convertLeapYear = arr => {
-    
+
+
+/**
+ * Ejercicio 4:
+ * Realizar una funcion que tome como parametro un string
+ * y retorne un array con todos caracteres del string
+ * 
+ * Resultado esperado: 
+ * ·> splitString('hola mundo')
+ * ·> ['h','o','l','a',' ','m','u','n','d','o']
+ * 
+ * @params 'String' - El String que queremos transformar
+ * @return [Array] - El array que se espera retornar
+ * 
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ */
+
+const splitString = (str) => {
+    return [...str]
 }
-
-
